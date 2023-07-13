4e7836c0d59c3cf69e7d8653182eddc19d92f6fd
Isthifaa <139338592+Isthifaa@users.noreply.github.com>
2023-07-13 16:46:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ document.getElementById("btn").onclick = function() {
     document.getElementById("listitem").appendChild(li);
     }
 }
+//add, delete, modify
+
+function add() {
+    var text = document.getElementById("newText").value;
+    var li = document.createElement("li");
+    if(text == "") {
+        alert("Please enter a task");
+    }else {
+    li.innerHTML = text;
+    document.getElementById("listitem").appendChild(li);
+    }
+}
+
+function del() {
+    var list = document.getElementById("listitem");
+    list.removeChild(list.childNodes[0]);
+}
+
+function mod() {
+    var text = document.getElementById("newText").value;
+    var list = document.getElementById("listitem");
+    list.childNodes[0].innerHTML = text;
+}
+//mark as done using javascript
+function done() {
+    var list = document.getElementById("listitem");
+    list.childNodes[0].style.textDecoration = "line-through";
+}
